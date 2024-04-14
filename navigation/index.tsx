6394d2526a20c4058907f22ede8f7fcc2c7031c7
dinhnguyetquynh <dquynhd202@gmail.com>
2024-04-14 14:09:17 +0700

    init router

---
+++ navigation/index.tsx
@@ -0,0 +1,18 @@
+import {NavigationContainer} from '@react-navigation/native';
+import React from 'react';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {Login} from '../screens';
+
+const Stack = createNativeStackNavigator();
+
+const AppRouters = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator {...{screenOptions: {headerShown: false}}}>
+        <Stack.Screen name="Login" component={Login} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default AppRouters;
