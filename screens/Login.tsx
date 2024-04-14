fa149122876f7dcd1a8341c1a83767d4a92ebf3f
dinhnguyetquynh <dquynhd202@gmail.com>
2024-04-14 15:20:25 +0700

    mail_pass_login

--- screens/Login.tsx
+++ screens/Login.tsx
@@ -1,13 +1,30 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {View, TextInput, Button} from 'react-native';
 
 const Login = () => {
+  const [mail, setMail] = useState('');
+  const [pass, setPass] = useState('');
+
+  const handleClick = () => {
+    console.log({mail});
+    console.log({pass});
+  };
   return (
     <View>
-      <TextInput placeholder="nhap email" />
-      <TextInput placeholder="nhap password" />
-      <Button title="Đăng nhập" color="#f194ff" />
+      <TextInput
+        placeholder="nhap email"
+        onChangeText={newValue => setMail(newValue)}
+        defaultValue={mail}
+      />
+      <TextInput
+        placeholder="nhap password"
+        onChangeText={newPass => setPass(newPass)}
+        defaultValue={pass}
+        secureTextEntry
+      />
+      <Button title="Đăng nhập" color="#f194ff" onPress={handleClick} />
     </View>
   );
 };
+
 export default Login;
