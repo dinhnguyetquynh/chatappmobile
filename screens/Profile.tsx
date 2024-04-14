4723ac36d58ee1576b69d79a5cfbb0a734cab231
dinhnguyetquynh <dquynhd202@gmail.com>
2024-04-14 18:19:19 +0700

    create home screens

---
+++ screens/Profile.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const Profile = () => {
+  return (
+    <View>
+      <Text>Pf</Text>
+    </View>
+  );
+};
+
+export default Profile;
